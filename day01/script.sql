cc10556d28e7eb626aa6686694161d7b5a81ea81
Janko <janko.chavannes@riviq.nl>
2022-12-11 11:31:25 +0100

    cleanup sql solution day1

--- day01/script.sql
+++ day01/script.sql
@@ -1,16 +1,30 @@
+USE [AdventOfCode2022] 
+GO
+
+CREATE SCHEMA [day1]
+GO
+
+CREATE TABLE [day1].[input](
+    content varchar(50) NULL,
+    line_nr INT IDENTITY(1,1) NOT NULL
+)
+GO
+
+/* Load data from flatfile into column "content" */
+
 WITH parsed as (
-	SELECT [Column 0] as content
-		, CASE WHEN [Column 0] = '' THEN 1 ELSE 0 END AS separator
-		, rid
-		, CAST([Column 0] AS INT) as calories
-	FROM [AdventOfCode2022].[dbo].[input1]
+    SELECT line_nr
+        ,content
+        ,CASE WHEN content = '' THEN 1 ELSE 0 END AS separator
+        ,CAST(content AS INT) as calories
+    FROM [day1].[input]
 ), groups AS (
-	SELECT rid, content, separator, calories, SUM(separator) OVER (ORDER BY rid ASC) AS groupid
-	FROM parsed
+    SELECT line_nr, content, separator, calories, SUM(separator) OVER (ORDER BY line_nr ASC) AS groupid
+    FROM parsed
 ), group_totals AS (
-	SELECT groupid, SUM(calories) AS total_calories, ROW_NUMBER() OVER (ORDER BY SUM(calories) DESC) as rn
-	FROM groups
-	GROUP BY groupid
+    SELECT groupid, SUM(calories) AS total_calories, ROW_NUMBER() OVER (ORDER BY SUM(calories) DESC) as rn
+    FROM groups
+    GROUP BY groupid
 )
 SELECT '1' as part, total_calories
 FROM group_totals
