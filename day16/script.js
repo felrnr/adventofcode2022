612d726a5d73623379b3f78d40ae5b8fd737c779
Janko <janko.chavannes@riviq.nl>
2022-12-16 22:14:33 +0100

    Much better time complexity. Making use of shortest path and moving directly to goal instead of checking each step along the way. Still O(n!) but not O(n^m)

--- day16/script.js
+++ day16/script.js
@@ -6,9 +6,9 @@ const data = fs.readFileSync(filepath).toString().trimEnd();
 
 let valves = data.split("\n")
   .map(l => /^Valve ([A-Z]{2}) has flow rate=(\d+);.+valves? (.+)/.exec(l))
-  // .map(([,valve,rate, connections]) => ({valve, rate, connections: connections.split(", ")}));
   .reduce((dict, [,id,rate,links]) => ({
     [id]: {
+      id,
       rate: parseInt(rate), 
       connections: Object.fromEntries(links.split(", ").map(c => ([c,1])))
     },
@@ -35,6 +35,31 @@ Object.keys(valves).filter(delId => valves[delId].rate === 0).forEach((delId) =>
 });
 
 
+function setShortestPaths(node) {
+  // Use variant of dijkstra's algorithm
+  let dist = Object.fromEntries(Object.keys(valves).map(k => ([k, k===node ? 0 : (valves[node].connections[k] ?? 999)])));
+  let Q = Object.entries(dist).filter(([,d]) => d > 0).map(([k]) => k);
+  let prev = Object.fromEntries(Object.entries(dist).map(([k,v]) => [k, v===999 ? null : node]));
+  
+  while (Q.length > 0) {
+      let u = Q.sort((a,b) => dist[a]-dist[b])[0];
+      Q.shift();
+
+      Q.filter(v => valves[u].connections[v] !== undefined).forEach(v => {
+          let alt = dist[u] + valves[u].connections[v];
+          if (alt < dist[v]) {
+              dist[v] = alt
+              prev[v] = u
+          }
+      })
+  }
+  delete dist[node];
+  delete dist[startId];
+  valves[node].connections = dist;
+}
+
+Object.keys(valves).forEach(setShortestPaths);
+
 // Part 1
 // Return the max flow pressure release by following this path
 
@@ -42,41 +67,44 @@ Object.keys(valves).filter(delId => valves[delId].rate === 0).forEach((delId) =>
 const idMapping = Object.fromEntries(Object.keys(valves).map((k,i) => [k, 2**i]));
 valves = Object.fromEntries(
   Object.entries(valves)
-    .map(([k, {rate, connections}]) => [
+    .map(([k, {id, rate, connections}]) => [
     idMapping[k], 
     {
+      id,
       rate, 
       connections: Object.fromEntries(Object.entries(connections).map(([k,v])=>[idMapping[k],v])),
     }
   ])
 );
 
-function visit(trace, openValves, timeLeft, flowRate, curValveId) {
-  if (timeLeft <= 1) return flowRate*timeLeft;
-  
-  let maxRelease = flowRate;
+let counter = 0;
+function visit(openValves, timeLeft, flowRate, curValveId) {
+  if (counter++%1000 === 0) console.log(`Counter: ${counter}`);
 
-  // Check self if not opened.
-  if ((openValves & curValveId) === 0) {
-    let extraFlow = valves[curValveId].rate;
-    let release = visit(curValveId, openValves|curValveId, timeLeft-1, flowRate+extraFlow, curValveId);
-    // let release = visit([curValveId], [curValveId, ...openValves], timeLeft-1, flowRate+extraFlow, curValveId);
-    if (release + flowRate > maxRelease) maxRelease = release + flowRate;
-  }
+  // Default is do nothing and wait.
+  let maxRelease = flowRate*timeLeft;
+  let route = [];
   
-  // Check connections
+  // Find best connection
   Object.entries(valves[curValveId].connections).map(([toId, dist]) => [parseInt(toId), dist])
-    .filter(([toId, dist]) =>  ((trace&toId) === 0 && timeLeft > dist)) // Not visited and time to reach
+    .filter(([toId, dist]) =>  ((openValves&toId)===0 && timeLeft > dist)) // Not visited and time to reach
     .forEach(([toId, dist]) => {
-      let release = visit(trace|curValveId, openValves, timeLeft-dist, flowRate, toId);
-      // let release = visit([...trace, curValveId], [...openValves], timeLeft-dist, flowRate, toId);
-      if (release + dist*flowRate > maxRelease) maxRelease = release + dist*flowRate 
+      let extraFlow = valves[toId].rate; // from opening new valves
+      let [release, r] = visit(openValves|toId, timeLeft-(dist+1), flowRate+extraFlow, toId);
+      let travelFlow = (dist+1)*flowRate; // accumulated during travel
+
+      if (release + travelFlow > maxRelease) {
+        maxRelease = release + travelFlow;
+        route = r;
+      }
     });
 
-    return maxRelease;
+    return [maxRelease, [...route, curValveId]];
 }
 
 console.log((new Date()));
-let pressureReleasedfast = visit(idMapping[startId],0,30,0,idMapping[startId]);
+let [pressureReleasedfast, route] = visit(0,30,0,idMapping[startId]);
 console.log(`total pressure released: ${pressureReleasedfast}`);
+console.log(`final counter: ${counter}`);
 console.log((new Date()));
+route.forEach(id => console.log("Visited node: " + valves[id].id));
